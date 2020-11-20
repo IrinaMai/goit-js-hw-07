9b47d29fd48b805c16c99f25c2bf2a2238338192
IrinaMai <irinam3340@gmail.com>
2020-11-20 15:36:39 +0300

    main part of hw

--- js/task-5.js
+++ js/task-5.js
@@ -1,29 +1,12 @@
-// Напиши класс Car с указанными свойствами и методами.
 
 class Car {
-  /*
-   * Добавь статический метод `getSpecs(car)`,
-   * который принимает объект-машину как параметр и выводит
-   * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
-   */
-    static getSpecs(car) {
-        console.log(`MaxSpeed: ${this.maxSpeed}, 
-    Speed: ${this.speed}, On or Off: ${this.isOn}, 
-    Distance: ${this.distance}, Price: ${this.price}.`);
-}
-    
 
-  /*
-   * Конструктор получает объект настроек.
-   *
-   * Добавь свойства будущеего экземпляра класса:
-   *  speed - текущая скорость, изначально 0
-   *  price - цена автомобиля
-   *  maxSpeed - максимальная скорость
-   *  isOn - заведен ли автомобиль, значения true или false. Изначально false
-   *  distance - общий киллометраж, изначально 0
-   */
-    constructor({speed = 0, price, maxSpeed, isOn = false, distance =0 }) {
+    static getSpecs(car) {
+        console.log(`MaxSpeed: ${car.maxSpeed}, 
+    Speed: ${car.speed}, IsOn: ${car.isOn}, 
+    Distance: ${car.distance}, Price: ${car.price}.`);
+    }
+    constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 }) {
         this.speed = speed;
         this._price = price;
         this.maxSpeed = maxSpeed;
@@ -38,77 +21,56 @@ class Car {
         return this._price
     }
 
-  /*
-   * Добавь геттер и сеттер для свойства price,
-   * который будет работать с свойством цены автомобиля.
-   */
-
-  /*
-   * Добавь код для того чтобы завести автомобиль
-   * Записывает в свойство isOn значение true
-   */
     turnOn() {
-        this.isOn = true; 
-  }
+        return this.isOn = true;
+    }
 
-  /*
-   * Добавь код для того чтобы заглушить автомобиль
-   * Записывает в свойство isOn значение false,
-   * и сбрасывает текущую скорость в 0
-   */
     turnOff() {
         this.isOn = false;
-  }
+        this.speed = 0;
 
-  /*
-   * Добавялет к свойству speed полученное значение,
-   * при условии что результирующая скорость
-   * не больше чем значение свойства maxSpeed
-   */
+    }
+
     accelerate(value) {
-        if (this.maxSpeed >= (this.speed + value)) {
-            return this.speed += value;
-        } else {
-            return this.maxSpeed;
-        }
-        
-  }
+        if (this.isOn) {
+            if ((this.speed + value) >= this.maxSpeed) {
+                return this.speed = this.maxSpeed;
+            } else {
+                return this.speed += value;
+            }
+        } else console.log("Turn on your car");
+    }
 
-  /*
-   * Отнимает от свойства speed полученное значение,
-   * при условии что результирующая скорость не меньше нуля
-   */
     decelerate(value) {
-        (0 >= (this.speed - value)) ? 0 : this.speed -= value;
-      
-  }
+        if ((this.speed - value) <= 0) {
+            return this.speed = 0
+        } else {
+            return this.speed -= value
+        }
 
-  /*
-   * Добавляет в поле distance киллометраж (hours * speed),
-   * но только в том случае если машина заведена!
-   */
+    }
+
     drive(hours) {
-        return this.distance = this.speed * hours
-        
-  }
+        return this.distance += this.speed * hours
+    }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
-// console.log(mustang);
-// console.log(mustang.price);
 
-// mustang.drive(2);
+// mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
 
 Car.getSpecs(mustang);
-// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
 
 mustang.decelerate(20);
-// mustang.drive(1);
-// mustang.turnOff();
+mustang.drive(1);
+mustang.turnOff();
 
-// Car.getSpecs(mustang);
-// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
 
-// console.log(mustang.price); // 2000
-// mustang.price = 4000;
-// console.log(mustang.price); // 4000
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
